140789bde46d27545f63cfa02a70c0144877c092
Nathan Seither <nathanseither@gmail.com>
2014-06-16 18:29:10 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunchables::Application.config.secret_token = '1eb31f5bd6277cb19083fd00fda0636e44e0b0f14f3a7d7a6034c19f2c14822f2877ee92ffee51dbc146364059fe02796d6d11291948bd8e2d379528f213818e'
